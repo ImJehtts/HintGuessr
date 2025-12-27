9e5f1c81ad329399e8759f68337d80875c29c11d
Mohib Rehman <rehmanmohib08@gmail.com>
2025-12-27 17:14:26 -0500

    First HintGuessr Commit. Was working on a different project but changed it to this hence the late intial commit

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import HintCard from './components/HintCard';
 
 export default function App() {
+
+  const [hint1, setHint1] = useState('');
+  const [hint2, setHint2] = useState('');
+  const [hint3, setHint3] = useState('');
+  const [hint4, setHint4] = useState('');
+  const [hint5, setHint5] = useState('');
+  const [hint6, setHint6] = useState('');
+  const [hint7, setHint7] = useState('');
+  const [hint8, setHint8] = useState('');
+  const [hint9, setHint9] = useState('');
+  const [hint10, setHint10] = useState(''); 
+  const [answer, setAnswer] = useState('');
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Welcome to HintGuessr!</Text>
+      </View>
+      <View style={styles.hintcards}>
+        <HintCard text={'Hint 1'}/>
+        <HintCard text={'Hint 2'}/>
+        <HintCard text={'Hint 3'}/>
+        <HintCard text={'Hint 4'}/>
+        <HintCard text={'Hint 5'}/>
+        <HintCard text={'Hint 6'}/>
+        <HintCard text={'Hint 7'}/>
+        <HintCard text={'Hint 8'}/>
+        <HintCard text={'Hint 9'}/>
+        <HintCard text={'Hint 10'}/>
+        <HintCard text={'Answer'}/>
+      </View>
+      <View style={styles.buttonsection}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => console.log('Next')}
+          >
+          <Text style={styles.buttonText}>Next Round</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
+const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#dcb79d',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    paddingTop: windowHeight > 700 ? 70 : 30,
+    paddingBottom: 30,
+    alignItems: 'center', 
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#374f3f',
+  },
+  hintcards: {
+    marginTop: windowHeight > 700 ? 85 : 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+buttonsection: {
+  flexDirection: 'row', 
+  justifyContent: 'space-between',
+  alignItems: 'center', 
+  marginTop: 10,
+  width: '80%', 
+},
+button: {
+  backgroundColor: '#374f3f',
+  paddingVertical: 15,
+  paddingHorizontal: 20,
+  borderRadius: 10,
+  alignItems: 'center',
+  justifyContent: 'center',
+  flex: 1, 
+  marginHorizontal: 5,
+},
+buttonText: {
+  color: '#ffffff',
+  fontSize: 20,
+  fontWeight: 'bold',
+  textAlign : 'center',
+},
 });
---
+++ components/HintCard.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, Switch} from 'react-native';
+
+const HintCard= (props) => {
+    const [showText, setShowText] = useState(true);
+
+    return (
+        <View style={styles.row}>
+             <View
+            style={[
+            styles.card,
+            { backgroundColor: showText ? '#572618' : '#bd764c' }
+            ]}
+            >
+            <Text style={styles.hintText}>
+                {showText ? props.text : ' '}
+            </Text>
+            </View>
+            <Switch
+            value={showText}
+            onValueChange={setShowText}
+            />
+      </View>
+    );
+}
+const windowHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+      },
+    card: {
+        borderRadius: 10,
+        padding: 5,
+        marginVertical: windowHeight > 700 ? 20 : 10,
+        marginTop: 0,
+        marginRight: 20,
+        width: 250,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    hintText: {
+        fontSize: windowHeight > 700 ? 20 : 15,
+        color: '#fff',
+        paddingTop: 5,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+});
+
+export default HintCard;
